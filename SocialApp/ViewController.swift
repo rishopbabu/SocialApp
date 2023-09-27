e76ad3a4b6efb296a1d2f9928f1fc96ff39bff34
Rishop Babu <rishopbabu22@gmail.com>
2023-09-28 04:45:45 +0530

    Initial Commit

---
+++ SocialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialApp
+//
+//  Created by Mac-OBS-51 on 28/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
