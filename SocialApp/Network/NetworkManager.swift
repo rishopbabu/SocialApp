57caac51a7154f6a65b0f81b79af6b245818202a
Rishop Babu <rishopbabu22@gmail.com>
2023-09-28 14:00:20 +0530

    network manager structure changed

---
+++ SocialApp/Network/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  SocialApp
+//
+//  Created by Mac-OBS-51 on 28/09/23.
+//
+
+import Foundation
---
+++ SocialApp/Network/NetworkRequestData.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkRequestData.swift
+//  SocialApp
+//
+//  Created by Mac-OBS-51 on 28/09/23.
+//
+
+import Foundation
